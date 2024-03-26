44da1f0c4fb6a7bb11b0ce175845dd65d6b92b27
LeightonSkervin <lskervin23@gmail.com>
2024-03-25 23:04:10 -0400

    lab complete

--- javascript/fibonacci.js
+++ javascript/fibonacci.js
@@ -1,6 +1,29 @@
 function fibonacci(num) {
-  // type your code here
+  let fibonacci = []; // Create an array to store Fibonacci sequence
+
+  // Check if num is 0
+  if (num === 0) {
+    fibonacci.push(0); // Push 0 to the fibonacci array
+    return fibonacci[num]
+  } else if (num === 1) {
+    fibonacci.push(0, 1); // Push 0 and 1 to the fibonacci array
+    return fibonacci[num]
+  } else if (num >= 2){
+    fibonacci.push(0, 1); // Push 0 and 1 to the fibonacci array
+    // make and append subsequent Fibonacci numbers
+    for (let i = 2; i < 29; i++) {
+      let fibNum = fibonacci[i - 1] + fibonacci[i - 2]; // Calculate Fibonacci number
+      fibonacci.push(fibNum); // Push Fibonacci number to the fibonacci array
+    }
+  }
+
+  return fibonacci[num]; // Return the fibonacci array containing Fibonacci sequence
 }
+
+
+
+
+
 
 if (require.main === module) {
   // add your own tests in here
